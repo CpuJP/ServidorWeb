c4107e06ffd3566c816b494ce9579375382a4630
CpuJP <cpujuanpis@gmail.com>
2023-02-12 13:49:03 -0500

    Se agrega OpenAPI

---
+++ src/main/java/com/example/saludo/Config/Swagger.java
@@ -0,0 +1,25 @@
+package com.example.saludo.Config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger {
+
+    //http://localhost:8080/swagger-ui/index.html#/ -> URL documentación
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Saludo API")
+                        .description("Aplicación Spring Boot para Saludar")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+}
